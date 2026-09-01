5e5a011223d300bf25d153f335261ec1230b4e7a
Dudz <eduardoqueirozcarvalho978@gmail.com>
2026-09-01 00:58:14 -0300

    not done yet

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
+IGNORED_DIRS = {
+    ".git",
+    "__pycache__",
+    "venv",
+    ".venv",
+    "node_modules"
+}
+
 from pathlib import Path
 
 
 def find_project(path):
     path = Path(path)
 
-    file_path = Path(path)
+    for ignored_dir in IGNORED_DIRS:
+        ignored_path = path / ignored_dir
+        if ignored_path.exists() and ignored_path.is_dir():
+            print(f"Ignored Directory: {ignored_dir}")
+
+        else:
+            print(f"Searching in: {path}")
 
     for item in path.iterdir():
         if item.is_file():
             print(f"File: {item.name}")
-        else:
-            print(f"Directory: {item.name}")
 
+        elif item.is_dir():
+            print(f"Directory: {item.name}")
+            find_project(item)  # Recursively search in subdirectories
+ 
 find_project("C:\\users\\Dudz\\Desktop\\CodePulse");
